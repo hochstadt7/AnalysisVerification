572373fde3e8c7b23893c09b18bae14107e57f6f
Admin <hochstadt@mail.tau.ac.il>
2021-08-01 12:22:12 +0300

    Assume abstract function

--- AnalysisVerification/src/Question1.java
+++ AnalysisVerification/src/Question1.java
@@ -18,71 +18,79 @@ public class Question1 extends Question {
 		if(command.equals("skip"))
 			return output; // state hasn't changed
 		
-		/* we need to discuss about assume*/
-		else if(command.contains("assume")) {
-			String afterAssume=command.substring(7);
-			String[] split=afterAssume.split(" ");
-			if(split[0].contains("!")) {
-				if((variables.get(split[0]).equals("EVEN")&&Integer.parseInt(split[2])%2==0)
-						||(variables.get(split[0]).equals("EVEN")&&Integer.parseInt(split[2])%2==1)) {
+		/* we need to discuss about assume a bit*/
+		else if(command.startsWith("assume")) {
+			String afterAssume=command.substring(7,command.length()-1); // get rid of "assume(" and ")"
+			String[] split=afterAssume.split(" "); // split[0]=left split[1]=action split[2]=right
+			if(split[1].contains("!")) { // inequality
+				
+				// condition satisfied, nothing changes
+				if((variables.get(split[0]).equals("EVEN")&&Integer.parseInt(split[2])%2==1)
+						||(variables.get(split[0]).equals("ODD")&&Integer.parseInt(split[2])%2==0)) {
 					return output;
 				}
-				else {
+				
+				else if(variables.get(split[0]).equals("TOP")) { // I am not sure what to do
+					for(String a: output.keySet()) {
+						output.put(a, "TOP");
+					}
+					return output;
+				}
+				
+				
+				else { //  this includes the case where the condition is not satisfied (or variables.get(split[0]) is BOTTOM(?))
 					for(String a: output.keySet()) {
 						output.put(a, "BOTTOM");
 					}
 					return output;
 				}
 			}
-			else {
-				if((variables.get(split[0]).equals("EVEN")&&Integer.parseInt(split[2])%2==1)
-						||(variables.get(split[0]).equals("EVEN")&&Integer.parseInt(split[2])%2==0)) {
-					return output;
+			else if(split[0].contains("=")){ // equality- we send to BOTTOM anyway? or TOP should send all to TOP? does it matter?
+				for(String a: output.keySet()) {
+					output.put(a, "BOTTOM");
 				}
-				else {
-					for(String a: output.keySet()) {
-						output.put(a, "BOTTOM");
-					}
-					return output;
-				}
+				return output;
 				
 			}
 			
 		}
 			
-		else if (command.contains("assert")) {
-			return output;
+		else if (command.startsWith("assert")) {
+			return output; // no change, we will check all asserts at the end of chaotic iteration
 		}
 		
 		else { // assignment
 			int equal=command.indexOf("=");
-			String afterEqual=command.substring(equal+2);
-			String beforeEqual=command.substring(0,equal-1);
+			String afterEqual=command.substring(equal+2); // we stepped over the space
+			afterEqual=afterEqual.substring(afterEqual.length()-1); // got rid of ")"
+			String beforeEqual=command.substring(1,equal-1); // got rid of "(" and the space
 			String[] splitAfter=afterEqual.split(" ");
 			if(afterEqual.contains("+")|| afterEqual.contains("-")) { // operator assignment
 				String tmp=variables.get(splitAfter[0]);
-				if(tmp.equals("TOP")||tmp.equals("BOTTOM")) {
-					
+				
+				if(tmp.equals("TOP")||tmp.equals("BOTTOM")) { // BOTTOM/TOP+1=BOTTOM/TOP
+					output.put(beforeEqual, tmp);
 				}
+				
 				else {
-					tmp= (variables.get(splitAfter[0]).equals("ODD")? "EVEN": "ODD");
+					tmp= (tmp.equals("ODD")? "EVEN": "ODD");
 					output.put(beforeEqual, tmp);
 				}
 				
 			}
 			
 			else if(afterEqual.contains("?")) {
-				output.put(beforeEqual, "TOP");
+				output.put(beforeEqual, "TOP"); // by definition
 			}
 			
 			else {
 				String tmp="";
-				try { // variable assignment
+				try { // constant assignment
 					int isNum=Integer.parseInt(splitAfter[0]);
 					tmp=(isNum%2==0?"EVEN":"ODD");
 				}
 				
-				catch(NumberFormatException e){ // constant assignment
+				catch(NumberFormatException e){ // variable assignment
 					tmp=variables.get(splitAfter[0]);
 				}
 				finally {
@@ -134,13 +142,13 @@ public class Question1 extends Question {
 		   matchList.add(regexMatcher.group(1));//Fetching Group from String
 		}
 		for(String str:matchList) {
-			   if(validate(str.substring(1,str.length()-1),last))
+			   if(validate(str.substring(1,str.length()-1),last)) // got rid of parenthesis
 				   return true; // it is enough that one conjunction will be true
 		}
 		return false;
 	}
 	
-	
+	// true only if all conditions are satisfied
 	public boolean validate(String andCondition, Vertex last) {
 		
 		String[] arr=andCondition.split(" ");
