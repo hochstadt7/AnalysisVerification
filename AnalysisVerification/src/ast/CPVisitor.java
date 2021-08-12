258e3745f68615cac398ff75f047a3735e115c7b
Admin <hochstadt@mail.tau.ac.il>
2021-08-12 18:30:13 +0300

    start remove sums

--- AnalysisVerification/src/ast/CPVisitor.java
+++ AnalysisVerification/src/ast/CPVisitor.java
@@ -7,11 +7,9 @@ public class CPVisitor implements Visitor {
     public static final Integer BOTTOM = Integer.MIN_VALUE;
 
     private final Map<String, Integer> inState;
-    private final Map<String, Map<String, Integer>> inSums;
     Map<String, Integer> newState;
-    Map<String, Map<String, Integer>> newSums;
     private final Map<String, Integer> allBottoms;
-    private final Map<String, Map<String, Integer>> allBottomsSums;
+    
 
     private Map<String, Integer> produceAllBottoms() {
         Map<String, Integer> bottoms = new HashMap<>();
@@ -21,41 +19,18 @@ public class CPVisitor implements Visitor {
         return bottoms;
     }
 
-    private Map<String, Map<String, Integer>> produceAllBottomsSums() {
-        Map<String, Map<String, Integer>> bottoms = new HashMap<>();
-        for (String var : inState.keySet()) {
-            Map<String, Integer> internal = new HashMap<>();
-            for(String var2 : inState.keySet()) {
-                if(!var.equals(var2)) {
-                    internal.put(var2, BOTTOM);
-                }
-            }
-            bottoms.put(var, internal);
-        }
-        return bottoms;
-    }
 
-    private void updateNewSums(String var1, String var2, Integer val) {
-        newSums.get(var1).put(var2, val);
-        newSums.get(var2).put(var1, val);
-    }
-
-    public CPVisitor(Map<String, Integer> inState, Map<String, Map<String, Integer>> inSums) {
+    public CPVisitor(Map<String, Integer> inState) {
         this.inState = inState;
-        this.inSums = inSums;
-        this.newState = new HashMap<>(inState);
-        this.newSums = new HashMap<>(newSums);
+        this.newState = new HashMap<>(inState);      
         this.allBottoms = produceAllBottoms();
-        this.allBottomsSums = produceAllBottomsSums();
+        
     }
 
     public Map<String, Integer> getNewState() {
         return newState;
     }
 
-    public Map<String, Map<String, Integer>> getNewSums() {
-        return newSums;
-    }
 
     @Override
     public void visit(AssertCmd assertCmd) {
@@ -68,73 +43,32 @@ public class CPVisitor implements Visitor {
     }
 
     @Override
-    public void visit(IntAssignCmd intAssignCmd) {
-        Map<String, Integer> internal = inSums.get(intAssignCmd.getLv());
+    public void visit(IntAssignCmd intAssignCmd) {       
         String var1 = intAssignCmd.getLv();
-
         if (intAssignCmd.isAnyNum()) { // i = ?
-            newState.put(var1, TOP);
-            for (String var2 : internal.keySet()) {
-                // if lv -> top, then lv-var -> top <=> var !-> bottom
-                updateNewSums(var1, var2, inState.get(var2).equals(BOTTOM) ? BOTTOM : TOP);
-            }
+            newState.put(var1, TOP);            
         } else { // i = K
             Integer val = intAssignCmd.getVal();
-            newState.put(var1, val);
-            updateSumsConstAssign(internal, var1, val);
-        }
-    }
-
-    // used to update all sums involving var1 with numerical value, based on var2's abs val
-    private void updateSumsConstAssign(Map<String, Integer> internal, String var1, Integer val) {
-        for (String var2 : internal.keySet()) {
-            Integer currAbsVal2 = inState.get(var2);
-            if (currAbsVal2.equals(TOP)) {
-                updateNewSums(var1, var2, TOP);
-            } else if (currAbsVal2.equals(BOTTOM)) {
-                updateNewSums(var1, var2, BOTTOM);
-            } else {
-                updateNewSums(var1, var2, currAbsVal2 + val);
-            }
+            newState.put(var1, val);   
         }
     }
 
     @Override
     public void visit(VarAssignCmd varAssignCmd) {
         String var1 = varAssignCmd.getLv();
-        Map<String, Integer> internal = inSums.get(var1);
         if (varAssignCmd.getType() == VarAssignCmd.AssignType.SIMPLE) { // i = j
             Integer newAbsVal1 = inState.get(varAssignCmd.getRv());
             newState.put(var1, newAbsVal1);
-            for (String var2 : internal.keySet()) {
-                Integer currAbsVal2 = inState.get(var2);
-                boolean eitherIsBottom = newAbsVal1.equals(BOTTOM) || currAbsVal2.equals(BOTTOM);
-                boolean eitherIsTop = newAbsVal1.equals(TOP) || currAbsVal2.equals(TOP);
-                if (eitherIsBottom) {
-                    updateNewSums(var1, var2, BOTTOM);
-                } else if (eitherIsTop) { // neither is bottom
-                    updateNewSums(var1, var2, TOP);
-                } else {
-                    updateNewSums(var1, var2, currAbsVal2 + newAbsVal1);
-                }
-            }
         } else { // increment / decrement
             int add = varAssignCmd.getType() == VarAssignCmd.AssignType.INCREMENT ? 1 : -1;
             Integer rvAbsVal = inState.get(varAssignCmd.getRv());
             if (rvAbsVal.equals(TOP)) {
                 newState.put(var1, TOP);
-                for (String var2 : internal.keySet()) {
-                    Integer newSum = inState.get(var2).equals(BOTTOM) ? BOTTOM : TOP;
-                    updateNewSums(var1, var2, newSum);
-                }
             } else if (rvAbsVal.equals(BOTTOM)) {
                 newState.put(var1, BOTTOM);
-                for (String var2 : internal.keySet()) {
-                    updateNewSums(var1, var2, BOTTOM);
-                }
             } else {
                 Integer newAbsVal1 = rvAbsVal + add;
-                updateSumsConstAssign(internal, var1, newAbsVal1);
+                newState.put(var1, newAbsVal1);
             }
         }
     }
@@ -168,10 +102,8 @@ public class CPVisitor implements Visitor {
             Integer currAbsVal1 = inState.get(var1);
             if (currAbsVal1.equals(BOTTOM)) {
             	newState = new HashMap<>(allBottoms);
-                newSums = new HashMap<>(allBottomsSums);
             } else  { // includes the cases where i is TOP or number
             	newState.put(var1, val);
-                updateSumsConstAssign(internal, var1, val);
             }
         }
     }
